c18f743492555b2a0236a70ec9d1dadca361ca35
Oscar Mora <oscare.ramirez@alumnos.udg.mx>
2022-03-12 16:28:29 -0600

    Add better heroes ;))

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,20 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice', points: 0 },
-      { id: 12, name: 'Narco', points: 3 },
-      { id: 13, name: 'Bombasto', points: 0 },
-      { id: 14, name: 'Celeritas', points: 7 },
-      { id: 15, name: 'Magneta', points: 4 },
-      { id: 16, name: 'RubberMan', points: 0 },
-      { id: 17, name: 'Dynama', points: 1 },
-      { id: 18, name: 'Dr IQ', points: 0 },
-      { id: 19, name: 'Magma', points: 2 },
-      { id: 20, name: 'Tornado', points: 9 },
+      { id: 11, name: 'El Ferras', points: 6 },
+      { id: 12, name: 'Laura Bozzo', points: 3 },
+      { id: 13, name: 'Dr. Simi', points: 0 },
+      { id: 14, name: 'Chuacheneger', points: 7 },
+      { id: 15, name: 'Pedrito Sola', points: 4 },
+      { id: 16, name: 'Gobesponja', points: 0 },
+      { id: 17, name: 'Hijo del papá', points: 1 },
+      { id: 18, name: 'El Venganzas', points: 0 },
+      { id: 19, name: 'Tío Ben', points: 2 },
+      { id: 20, name: 'Marco Aurelio', points: 9 },
+      { id: 21, name: 'Chica 13', points: 4 },
+      { id: 22, name: 'Pati Chapoy', points: 1 },
+      { id: 23, name: 'Master Chef', points: 1 },
+      { id: 24, name: 'Shinji', points: 7 },
     ];
     return { heroes };
   }
