5516bb4c2bea993a2d1f637bea1620e04dd917af
Oscar Mora <oscare.ramirez@alumnos.udg.mx>
2022-03-12 15:11:31 -0600

    Add hero points + list sorting

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,15 +21,20 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.heroService
+      .getHeroes()
+      .subscribe(
+        (heroes) => (this.heroes = heroes.sort((a, b) => a.points - b.points))
+      );
   }
 
   add(name: string): void {
     name = name.trim();
     if (!name) return;
 
-    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+    this.heroService.addHero({ name, points: 0 } as Hero).subscribe((hero) => {
       this.heroes.push(hero);
+      this.getHeroes();
     });
   }
 
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,16 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' },
+      { id: 11, name: 'Dr Nice', points: 0 },
+      { id: 12, name: 'Narco', points: 3 },
+      { id: 13, name: 'Bombasto', points: 0 },
+      { id: 14, name: 'Celeritas', points: 7 },
+      { id: 15, name: 'Magneta', points: 4 },
+      { id: 16, name: 'RubberMan', points: 0 },
+      { id: 17, name: 'Dynama', points: 1 },
+      { id: 18, name: 'Dr IQ', points: 0 },
+      { id: 19, name: 'Magma', points: 2 },
+      { id: 20, name: 'Tornado', points: 9 },
     ];
     return { heroes };
   }
